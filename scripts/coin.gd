a3ff487de03ab094dd5b975134c412c1614c43e1
Fratm <fratm@fratm.com>
2023-02-17 15:37:21 -0800

    Coins ding now.

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-
+var dingPlaying = false
 
 func _ready():
 	pass # Replace with function body.
@@ -8,10 +8,19 @@ func _process(delta):
 	if Globals.gameOver:
 		queue_free()
 	var velocity = Vector2(0, Globals.scrollSpeed) * delta
-	var collision = move_and_collide(velocity)
-	if collision and collision.collider.name == "Player":
-		Globals.totalDistance += 10
-		queue_free()
+	var _collision = move_and_collide(velocity)
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	if body.name != "Player":
+		return
+	Globals.totalDistance += 10
+	$AudioStreamPlayer2D.play()
+	yield(get_tree().create_timer(.75), "timeout")
+	$AudioStreamPlayer2D.stop()
+	queue_free()
+
+	pass # Replace with function body.
